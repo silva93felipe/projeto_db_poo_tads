4038091fd00b74d14379a8428372a45ddcf25f8b
unknown <silva93felipe@gmail.com>
2021-11-21 23:09:43 -0300

    Ajuste na classe pessoa

--- projeto_hospital/src/projeto_hospital/Hospital.java
+++ projeto_hospital/src/projeto_hospital/Hospital.java
@@ -1,6 +1,10 @@
 package projeto_hospital;
 
-public class Hospital extends RedeHospital{
+public class Hospital {
+	private String telefone;
+	private String email;
+	private String endereco;
+	private int redeHospital;
 	
 	public Hospital() {	}
 	
